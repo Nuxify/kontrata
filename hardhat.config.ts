448974ca73c38f9b91fe63b1b8d2c3b8012ebcff
Karl Anthony Baluyot <sovietkamarov@gmail.com>
2022-07-03 19:58:53 +0800

    chore: format and apply prettier

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,17 +1,17 @@
-import * as dotenv from 'dotenv'
+import * as dotenv from "dotenv"
 
-import { HardhatUserConfig, task } from 'hardhat/config'
-import '@nomiclabs/hardhat-etherscan'
-import '@nomiclabs/hardhat-waffle'
-import '@typechain/hardhat'
-import 'hardhat-gas-reporter'
-import 'solidity-coverage'
+import { HardhatUserConfig, task } from "hardhat/config"
+import "@nomiclabs/hardhat-etherscan"
+import "@nomiclabs/hardhat-waffle"
+import "@typechain/hardhat"
+import "hardhat-gas-reporter"
+import "solidity-coverage"
 
 dotenv.config()
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners()
 
   for (const account of accounts) {
@@ -24,7 +24,7 @@ task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: '0.8.15',
+    version: "0.8.15",
     settings: {
       optimizer: {
         enabled: true,
@@ -34,17 +34,17 @@ const config: HardhatUserConfig = {
   },
   networks: {
     ganache: {
-      url: 'http://127.0.0.1:8545'
+      url: "http://127.0.0.1:8545",
     },
     rinkeby: {
-      url: process.env.RINKEBY_RPC || '',
+      url: process.env.RINKEBY_RPC || "",
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
   },
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
-    currency: 'USD',
+    currency: "USD",
   },
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY,
