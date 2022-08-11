48702043cb0d0d4c80575a22f19a5e1faf363373
Karl Anthony Baluyot <sovietkamarov@gmail.com>
2022-08-11 18:03:36 +0800

    test: change message

--- test/index.ts
+++ test/index.ts
@@ -9,11 +9,11 @@ describe("Greeter", function () {
 
     expect(await greeter.greet()).to.equal("Hello, world!")
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!")
+    const setGreetingTx = await greeter.setGreeting("Kontrata from Nuxify")
 
     // wait until the transaction is mined
     await setGreetingTx.wait()
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!")
+    expect(await greeter.greet()).to.equal("Kontrata from Nuxify")
   })
 })
