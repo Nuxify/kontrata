4783b96002777a3113d2467757ee2b9bf63f795c
Karl Anthony Baluyot <sovietkamarov@gmail.com>
2023-10-04 22:56:30 +0800

    build: upgrade to solidity 0.8.19

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -24,7 +24,7 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: "0.8.17",
+    version: "0.8.19",
     settings: {
       optimizer: {
         enabled: true,
